a7e9dd94ab3baf05d5b07ba93b5304784c1b1111
Mohammad Al-Ahdal <mo@mohammad.dev>
2019-08-28 23:25:58 -0700

    add project scaffolding and export a Link component. Setup webpack as well

---
+++ src/Link/Link.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export class Link extends Component {
+    static propTypes = {
+
+    }
+
+    render() {
+        return (
+            <div>
+                This is a temporary link
+            </div>
+        )
+    }
+}
+
+export default Link
